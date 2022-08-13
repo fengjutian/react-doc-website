27fabea9c96a20490e50814d68b8307ea1c48d9a
fengjutian <fengjutian@fengjutiandeMacBook-Pro.local>
2022-08-13 22:24:28 +0800

    save code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'React Doc',
+  tagline: 'React Doc 中文文档',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'React Doc',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -120,7 +120,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Fengjutian.`,
       },
       prism: {
         theme: lightCodeTheme,
